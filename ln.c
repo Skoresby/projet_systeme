905cd64e93d90057a25c973f78fbbbd0d4e90946
OceaneFlam <34216308+OceaneFlam@users.noreply.github.com>
2018-06-15 10:31:31 +0200

    Update ln.c

--- ln.c
+++ ln.c
@@ -1,28 +1,30 @@
-#include "structInode.h"
-#include "liste_chainee.h"
-#include "variable_environnement.h"
+#include "ln.h"
 #include <string.h>
 #include <stdbool.h>
+#include "path.h"
+#include "liste_chainee.h"
+#include "variable_environnement.h"
+#include "verifExistence.h"
+#include "verifType.h"
+#include "vg.h"
 
-extern VE ve;
-extern Inode *tabInode;
-
+extern VE VAR_E;
 
 void ln(char * argv[])
 {
 	Path arg1, arg2;
-	arg1=path(argv[1], 0);//0 pour demander le chemin si celui ci n est pas rempli
+	arg1=path(argv[1], 0);/*0 pour demander le chemin si celui ci n est pas rempli*/
 	arg2=path(argv[2], 1);
-	if(arg2.chemin==NULL)//si le deuxieme argument n a pas de chemin, on le place dans le repertoire courant
+	if(arg2.chemin==NULL)/*si le deuxieme argument n a pas de chemin, on le place dans le repertoire courant*/
 	{
-		arg2.chemin=(char*)malloc(strlen(ve.mypath)*sizeof(char));
-		strcpy(arg2.chemin, ve.mypath);
+		arg2.chemin=(char*)malloc(strlen(VAR_E.mypath)*sizeof(char));
+		strcpy(arg2.chemin, VAR_E.mypath);
 	}
 	
 	int ninode2; 
 	ninode2=verifExistence(arg2);
 	
-	if(ninode2!=-1)// l existance de arg1 est verifiee avec verifType!
+	if(ninode2!=-1)/* l existance de arg1 est verifiee avec verifType!*/
 	{
 		printf("probleme rencontre : le deuxieme argument existe\n");
 		exit(EXIT_FAILURE);
