c7f3afff8cbde0f2b6cd04840eb810eb53ec86ed
REKLYNDA <34442096+REKLYNDA@users.noreply.github.com>
2018-06-15 17:57:05 +0200

    Update myclose.c

--- myclose.c
+++ myclose.c
@@ -16,7 +16,8 @@ int myclose(int etat)
 		if (INODE[i].etat == etat) // on cherche dans le tableau d'inode l'inode qui a le meme etat que celui en argument (on verifie l'existence du fichier donc)
 		{
         		retour = close(etat);
-			INODE[i].etat = retour-1;
+			INODE[i].etat = retour-1; // nous avons pensé comme amélioration à remplacer etat par -etat en cas d'échec de fermeture
+			// du fichier, afin de retrouver ensuite le descripteur pour retenter un fermeture
 		}
 	}
 	return retour-1;
