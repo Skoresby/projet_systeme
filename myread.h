5730d772c767135af826f8a9866ed106fc731807
REKLYNDA <34442096+REKLYNDA@users.noreply.github.com>
2018-06-15 14:27:02 +0200

    Create myread.h

---
+++ myread.h
@@ -0,0 +1,17 @@
+#ifndef MYREAD_H
+#define MYREAD_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "inode.h"
+#include "vg.h"
+# include myopen.h"
+# include myclose.h"
+
+void myread(int ninode, char buf[] ,int nombre)
+
+#endif
