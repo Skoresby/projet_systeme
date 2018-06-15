02cfff2354b4547b5b2e54f6858d40c5ce37c4da
christine928 <34441350+christine928@users.noreply.github.com>
2018-06-15 22:47:54 +0200

    Update mywrite.h

--- mywrite.h
+++ mywrite.h
@@ -8,12 +8,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include "inode.h"
-#include "vg.h"
-# include "myopen.h"
-# include "myclose.h"
-#include "liste_chainee.h"
-#include "variable_environnement.h"
 
 
 void mywrite(int ninode,char buf[], int nombre);
