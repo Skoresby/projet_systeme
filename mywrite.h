f742bdc7bcc7408704746c41141ce2d55b15c576
christine928 <34441350+christine928@users.noreply.github.com>
2018-06-15 22:16:53 +0200

    Update mywrite.h

--- mywrite.h
+++ mywrite.h
@@ -10,8 +10,8 @@
 #include <fcntl.h>
 #include "inode.h"
 #include "vg.h"
-# include myopen.h"
-# include myclose.h"
+# include "myopen.h"
+# include "myclose.h"
 #include "liste_chainee.h"
 #include "variable_environnement.h"
 
