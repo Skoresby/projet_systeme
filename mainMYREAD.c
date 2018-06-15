b5e40edd858f48d9af942d7fff55b56e612aee1d
REKLYNDA <34442096+REKLYNDA@users.noreply.github.com>
2018-06-15 14:34:12 +0200

    Create mainMYREAD.c

---
+++ mainMYREAD.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "inode.h"
+#include "vg.h"
+# include myopen.h"
+# include myclose.h"
+
+
+extern Inode* INODE;
+
+int main(int argc, char* argv [])
+{
+	myread(ninode,buf,nombre);
+	return 0;
+}
