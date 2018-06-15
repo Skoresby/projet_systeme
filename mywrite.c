d51184c1764deca7a6bfbec82e2bd2baea8d5ba8
christine928 <34441350+christine928@users.noreply.github.com>
2018-06-15 22:16:19 +0200

    Update mywrite.c

--- mywrite.c
+++ mywrite.c
@@ -1,15 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include "inode.h"
-#include "vg.h"
-# include myopen.h"
-# include myclose.h"
-#include "liste_chainee.h"
-#include "variable_environnement.h"
+
+#include "mywrite.h"
 
 extern Inode* INODE;
 extern char** tabBlocs;
