a9967882a597bbb3d5daf542d69cf45474d75257
OceaneFlam <34216308+OceaneFlam@users.noreply.github.com>
2018-06-15 14:09:59 +0200

    Add files via upload

---
+++ vg.h
@@ -0,0 +1,13 @@
+#ifndef VG_H
+#define VG_H
+	#include "variable_environnement.h"
+	#include "inode.h"
+	#include "liste_chainee.h"
+	
+	VE VAR_E;
+	Inode* INODE;
+	char** tabBlocs;
+	L_Bloc * listeBloc;
+	L_Bloc * listeInode;
+	
+#endif
