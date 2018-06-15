ef056cea2ed8348ed498cf0b31bbf5385e271088
Skoresby <34441945+Skoresby@users.noreply.github.com>
2018-06-15 23:02:31 +0200

    Create ls.h
    pas test non plus

---
+++ ls.h
@@ -0,0 +1,12 @@
+#ifndef LS_H
+#define LS_H
+
+#include "vg.h"
+#include "verifExistence.h"
+#include "verifType.h"
+#include "myread.h"
+
+void ls (char *argv[]);
+void affiche_ls(char *repertoire, bool option);
+
+#endif
