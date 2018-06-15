94e3c315d2d9735a9421a9efd28268b51a4786b2
OceaneFlam <34216308+OceaneFlam@users.noreply.github.com>
2018-06-15 17:51:06 +0200

    Add files via upload

---
+++ myclose.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "myclose.h"
+#include "inode.h"
+#include "variable_environnement.h"
+#include "vg.h"
+
+extern Inode* INODE;
+extern VE VAR_E;
+
+int myclose(int etat)
+{
+	int i,retour=-1;
+	for (i=0;i<VAR_E.nbNinode;i++) //taille du tableau d'inode, a remplacer par la const globale représentant la taille du tableau d'inode
+	{
+		if (INODE[i].etat == etat) // on cherche dans le tableau d'inode l'inode qui a le meme etat que celui en argument (on verifie l'existence du fichier donc)
+		{
+        		retour = close(etat);
+			INODE[i].etat = retour-1;
+		}
+	}
+	return retour-1;
+}
+
