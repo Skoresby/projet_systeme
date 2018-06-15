15bd5a5a07acd3f97ac1a573007122e2184d5097
OceaneFlam <34216308+OceaneFlam@users.noreply.github.com>
2018-06-15 10:44:53 +0200

    Update verifExistence.c

--- verifExistence.c
+++ verifExistence.c
@@ -1,24 +1,27 @@
-#include "structInode.h"
-#include "variable_environnement.h"
 #include <stdbool.h>
 #include <string.h>
+#include "inode.h"
+#include "path.h"
+#include "liste_chainee.h"
+#include "variable_environnement.h"
+#include "vg.h"
 
-extern Inode *tabInode;
-extern VE ve;
-//extern int nbreInode;
 
-int verifExistence(Path chemin)
+extern Inode* INODE;
+extern VE VAR_E;
+
+int verifExistence(Path cheminTest)
 {
 	int i, count=0, rep;
-	int *correspondance=(int*)malloc(ve.nbNinode*sizeof(int));
-	int ninode=-1;//retourne -1 si n existe pas, ninode sinon
-	for(i=0; i<ve.nbNinode; i++)
+	int *correspondance=(int*)malloc(VAR_E.nbNinode*sizeof(int));
+	int ninode=-1;/*retourne -1 si n existe pas, ninode sinon*/
+	for(i=0; i<VAR_E.nbNinode; i++)
 	{
-		//if(strlen(chemin.chemin)!=0)//chemin non vide
-		if(chemin.chemin!=NULL)
+		/*if(strlen(chemin.chemin)!=0)//chemin non vide*/
+		if(cheminTest.chemin!=NULL)
 		{
-			//printf("tb.n=%s, c.n=%s, tb.c=%s, c.c=%s\n", tabInode[i].nom_fichier, chemin.nom, tabInode[i].chemin_absolu,  chemin.chemin);
-			if((strcmp(tabInode[i].nom_fichier, chemin.nom)==0)&&(strcmp(tabInode[i].chemin_absolu, chemin.chemin)==0))
+			/*printf("tb.n=%s, c.n=%s, tb.c=%s, c.c=%s\n", INODE[i].nom_fichier, chemin.nom, INODE[i].chemin_absolu,  chemin.chemin);*/
+			if((strcmp(INODE[i].nom_fichier, cheminTest.nom)==0)&&(strcmp(INODE[i].chemin_absolu, cheminTest.chemin)==0))
 			{
 				ninode=i;
 				break;
@@ -26,16 +29,16 @@ int verifExistence(Path chemin)
 		}
 		else
 		{
-			if(strcmp(tabInode[i].nom_fichier, chemin.nom)==0)
+			if(strcmp(INODE[i].nom_fichier, cheminTest.nom)==0)
 			{
-				printf("correspondance numéro %d : chemin=%s\n", count, tabInode[i].chemin_absolu);
+				printf("correspondance numéro %d : chemin=%s\n", count, INODE[i].chemin_absolu);
 				correspondance[count]=i;
 				count++;
 			}
 		}
 	}
 	
-	if(chemin.chemin==NULL)//chemin vide, on demande de choisir
+	if(cheminTest.chemin==NULL)/*chemin vide, on demande de choisir*/
 	{
 		do
 		{
