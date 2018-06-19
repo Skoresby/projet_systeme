050bc35fdf0be393d1336667e97585710cb50e46
Skoresby <34441945+Skoresby@users.noreply.github.com>
2018-06-19 18:35:57 +0200

    Create myls.h

---
+++ myls.h
@@ -0,0 +1,12 @@
+#ifndef MYLS_H
+#define MYLS_H
+
+#include "vg.h"
+#include "verifExistence.h"
+#include "verifType.h"
+#include "myread.h"
+
+void myls (char *argv[]);
+void affiche_ls(char *repertoire, bool option);
+
+#endif
