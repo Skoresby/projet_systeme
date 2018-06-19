9064221799ccaf206ce1ea614c41c0f919d6fcb8
Skoresby <34441945+Skoresby@users.noreply.github.com>
2018-06-19 20:15:49 +0200

    Add files via upload

--- myls.h
+++ myls.h
@@ -5,6 +5,9 @@
 #include "verifExistence.h"
 #include "verifType.h"
 #include "myread.h"
+#include "path.h"
+#include "liste_chainee.h"
+
 
 void myls (char *argv[]);
 void affiche_ls(char *repertoire, _Bool option);
